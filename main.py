29baa5b3d965d336f9ac32586b4110442d741bce
Vespirit <carmrest00@gmail.com>
2022-07-12 14:08:34 -0400

    Fixed formatting

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from gui import *
 from tkinter import filedialog
 
+
 def openfile() -> str:
     return filedialog.askdirectory()
+
 
 def restart_game(game: int, path: str) -> None:
     with open(path + "/data/user.data", "r") as f:
         lines = f.readlines()
-    
+
     lines[game + 1] = "NFM" + str(game) + "(0,1)\n"
 
     with open(path + "/data/user.data", "w") as f:
